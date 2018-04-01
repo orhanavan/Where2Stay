59a4eb14b30af8672607c057c5af5a959e2c69f0
orhan <o.avan.71@gmail.com>
2018-04-01 21:21:31 +0300

    ilan ekleme sayfaları üzerinde çalışıldı(tamamlanmadı)

--- app/src/main/java/com/w2s/orhan/where2stay/Advert/AddAdvert.java
+++ app/src/main/java/com/w2s/orhan/where2stay/Advert/AddAdvert.java
@@ -1,14 +1,48 @@
 package com.w2s.orhan.where2stay.Advert;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-import com.w2s.orhan.where2stay.MainActivity;
 import com.w2s.orhan.where2stay.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddAdvert extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    static final String[] PROVINCES = new String[]{
+            "Adana", "Adıyaman", "Afyon", "Ağrı",
+            "Amasya", "Ankara" , "Antalya", "Artvin",
+            "Aydın", "Balıkesir", "Bilecik", "Bingöl",
+            "Bitlis", "Bolu", "Burdur", "Bursa",
+            "Çanakkale", "Çankırı", "Çorum", "Denizli",
+            "Diyarbakır", "Edirne", "Elazığ", "Erzincan",
+            "Erzurum", "Eskişehir", "Gaziantep", "Giresun",
+            "Gümüşhane", "Hakkari", "Hatay", "Isparta",
+            "Mersin", "İstanbul", "İzmir", "Kars",
+            "Kastamonu", "Kayseri", "Kırklareli", "Kırşehir",
+            "Kocaeli", "Konya", "Kütahya", "Malatya",
+            "Manisa", "Kahramanmaraş", "Mardin", "Muğla",
+            "Muş", "Nevşehir", "Niğde", "Ordu",
+            "Rize", "Sakarya", "Samsun", "Siirt",
+            "Sinop", "Sivas", "Tekirdağ", "Tokat",
+            "Trabzon", "Tunceli", "Şanlıurfa", "Uşak",
+            "Yozgat", "Zonguldak", "Aksaray", "Bayburt",
+            "Karaman", "Kırıkkale", "Batman", "Şırnak",
+            "Bartın", "Ardahan", "Iğdır", "Yalova",
+            "Karabük", "Kilis", "Osmaniye", "Düzce", "Van"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +51,64 @@ public class AddAdvert extends AppCompatActivity {
 
         Intent intent = getIntent();
         String type = intent.getStringExtra("type");
-        TextView textView = findViewById(R.id.txt_Type);
-        textView.setText(type);
+
+        viewPager = findViewById(R.id.vp_advert);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new adv_oneFragment(), "kiralık");
+        viewPagerAdapter.addFragment(new adv_twoFragment(), "satılık");
+        viewPagerAdapter.addFragment(new adv_threeFragment(), "günlük kiralık daire");
+        viewPager.setAdapter(viewPagerAdapter);
+
+
+
+
+        Spinner spinner = findViewById(R.id.spinner);
+        final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.types, R.layout.spinner_item);
+        adapter.setDropDownViewResource(R.layout.spinner_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String text = adapterView.getItemAtPosition(i).toString();
+                //Toast.makeText(adapter.getContext(), text, Toast.LENGTH_SHORT).show();
+                viewPager.setCurrentItem(i,true);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
     }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }// viewpager adapter
 }
